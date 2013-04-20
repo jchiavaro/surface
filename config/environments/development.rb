7e73f51d858f418cc8e370dee2df7ed5fb5cfb34
mbeade <mbeade@velocitypartners.net>
2013-04-20 17:19:54 -0300

    removed public/assets generated. Added footer to the app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,9 @@ SurfaceApp::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
+
   # Do not compress assets
   config.assets.compress = false
 
