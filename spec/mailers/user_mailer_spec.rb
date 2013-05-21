f93d5ac22b029f7a0261f8b03029746bb61db9ff
Jorge Chiavaro <jorgechiavaro@gmail.com>
2013-05-21 10:59:27 -0300

    refactoring user mailer spec

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,22 +1,14 @@
 require "spec_helper"
 
 describe UserMailer do
-  describe "#welcome_email" do
-    before do
-      @user = mock("User", { id: 1, first_name: "Carl", email: "user@domain.com", auth_code: "secure_token" })
-    end
-    it "should send a welcome email" do
-      email = UserMailer.welcome_email(@user).deliver
-      assert !ActionMailer::Base.deliveries.empty?
-      assert_equal ["surfacenotifications@surface.com"], email.from
-      assert_equal [@user.email], email.to
-      assert_match /Welcome to Surface!, #{@user.first_name}/, email.encoded
-      assert_equal "Welcome to Surface!", email.subject
-    end
+  context "#welcome_email" do
+    let(:user) { stub(id: 1, first_name: "Carl", email: "user@domain.com", auth_code: "secure_token") }
+    subject{ UserMailer.welcome_email(user) }
 
-    it "Should assign the confirmation url" do
-      email = UserMailer.welcome_email(@user).deliver
-      email.body.encoded.should match("<a href='http://localhost:3000/users/#{@user.id}/account/#{@user.auth_code}'>Confirm Account</a>")
-    end
+    its(:from) { should include("surfacenotifications@surface.com") }
+    its(:to) { should include(user.email) }
+    its(:subject) { should == "Welcome to Surface!" }
+    its(:encoded) { should match("Welcome to Surface!, #{user.first_name}")}
+    its("body.encoded") { should match("<a href='http://localhost:3000/users/#{user.id}/account/#{user.auth_code}'>Confirm Account</a>") }
   end
 end
